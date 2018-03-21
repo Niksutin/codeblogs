0e084b301b140c0f91a12a3465e0d587b17b6183
Niksutin <niko.mattila@cs.tamk.fi>
2018-03-21 22:02:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Post from './Post.js';
+import Menu from "./Menu";
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Post />
+      <div className="App">
+        <header className="App-header">
+          <Menu title="Codeblogs"/>
+        </header>
+        <div className="App-posts">
+          <Post title="JavaScript basics!">Javascript is a programming language.</Post>
+          <Post title="HTML struggling...">HTML is short from Hyper Text Markup Language!</Post>
+        </div>
       </div>
     );
   }
