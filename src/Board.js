5bfdbdeaa3091184146961116395e2da9df8ca80
Niksutin <niko.mattila@cs.tamk.fi>
2018-03-22 00:49:30 +0200

    Update to simplify the posts array.

--- src/Board.js
+++ src/Board.js
@@ -4,27 +4,51 @@ import Post from './Post.js';
 export default class Board extends Component {
   constructor(props) {
     super(props);
-    this.state = {posts: [{
-          title: 'HTML is for nerds',
-          text: 'HTML is short from Hyper Text Markup Language!'
-        }, {
-          title: 'JavaScript basics!',
-          text: 'Javascript is a "programming" language.'
-        }, {
-          title: 'Java, beauty and the beast',
-          text: 'Swedish study finds out Java is the best language LOL'
-        }
+    this.state = {posts: [
+        'HTML is for nerds.',
+        'HTML is short from Hyper Text Markup Language!',
+        'JavaScript basics!',
+        'Javascript is a "programming" language.',
+        'Java, beauty and the beast.',
+        'Swedish study finds out Java is the best language LOL.'
       ]};
+    this.add = this.add.bind(this);
+    this.removePost = this.removePost.bind(this);
+    this.editPost = this.editPost.bind(this);
+    this.forEachPost = this.forEachPost.bind(this);
+  }
+
+  add(text) {
+    let array = this.state.posts;
+    array.push(text);
+    this.setState({posts: array});
+  }
+
+  removePost(i) {
+    let array = this.state.posts;
+    array.splice(i, 1);
+    this.setState({posts: array});
+  }
+
+  editPost(inputText, i) {
+    let array = this.state.posts;
+    array[i] = inputText;
+    this.setState({posts: array});
+  }
+
+  forEachPost(text, i) {
+    return (
+      <Post key={i} index={i} editPost={this.editPost} removePost={this.removePost}>
+        {text}
+      </Post>
+    )
   }
 
   render() {
     return (
         <div>
-          {
-            this.state.posts.map(function(post, i) {
-              return <Post key={i} title={post.title}>{post.text}</Post>;
-            })
-          }
+          <button onClick={this.add.bind(null, 'Default text')} className="PostButton" id="add">Add</button>
+          { this.state.posts.map(this.forEachPost) }
         </div>
     );
   }
