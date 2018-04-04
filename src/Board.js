0c8b39befaad34ec1643a6022d083767bd8f8728
Niksutin <niko.mattila@cs.tamk.fi>
2018-04-04 18:59:07 +0300

    Modify the way the application stores Post data. Add fetching from Backend

--- src/Board.js
+++ src/Board.js
@@ -6,65 +6,108 @@ export default class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [],
+      postData: [
+        {title: "test1", text: "text1", key: 0, editing: false},
+        {title: "test2", text: "text2", key: 1, editing: false},
+        {title: "test3", text: "text3", key: 2, editing: false}
+      ],
+      latestId: 0,
+      url: 'http://localhost:8080/posts'
     };
     this.add = this.add.bind(this);
     this.removePost = this.removePost.bind(this);
     this.editPost = this.editPost.bind(this);
-    this.testDataInject = this.testDataInject.bind(this);
+    this.fetchPosts = this.fetchPosts.bind(this);
   }
 
   componentDidMount() {
-    fetch('http://localhost:8080/posts')
-      .then(results => {
-        return results.json();
-      }).then(data => {
-        console.log("posts", data);
-        let fetchedPosts = data.map((post) => {
-          return post.content;
-        });
-        this.setState({posts: fetchedPosts});
-      }).catch(this.testDataInject());
+    this.fetchPosts();
   }
 
-  testDataInject() {
-    console.log("Error occurred in fetching data!");
-    let testPosts = [
-      "Test1",
-      "Test2",
-      "Test3"
-    ];
-    this.setState({posts: testPosts});
+  fetchPosts() {
+    let id = 0;
+    fetch(this.state.url)
+        .then(results => {
+          return results.json();
+        }).then(data => {
+      console.log("posts ", data);
+      let fetchedPosts = data.map((post) => {
+        console.log(post.id, " id");
+        id = post.id;
+        return {title: post.title, text: post.content, key: post.id, editing: false};
+      });
+      this.setState({postData: fetchedPosts, latestId: id});
+    }).catch(function(error) {
+      console.log(error);
+    });
   }
 
+  // This will be used later on
+  postPosts(data) {
+    fetch(this.state.url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: data.title,
+        content: data.text,
+        writer: 'testAuthor'
+      })
+    })
+    .then(function (data) {
+      console.log('Request success: ', data);
+    })
+    .catch(function (error) {
+      console.log('Request failure: ', error);
+    });
+  }
 
   add(text) {
-    let array = this.state.posts;
-    array.push(text);
-    this.setState({posts: array});
+    let array = this.state.postData;
+    array.push({title: text, text: "Default text", key: this.state.latestId + 1, editing: true});
+    this.setState({postData: array, latestId: this.state.latestId + 1});
   }
 
-  removePost(i) {
-    let array = this.state.posts;
-    array.splice(i, 1);
-    this.setState({posts: array});
+  removePost(id) {
+    let array = this.state.postData;
+    console.log(array[id], " array");
+    for(let i = 0; i < array.length; i++) {
+      if (array[i].key === id) {
+        array.splice(i, 1);
+      }
+    }
+    this.setState({postData: array});
   }
 
-  editPost(inputText, i) {
-    let array = this.state.posts;
-    array[i] = inputText;
-    this.setState({posts: array});
+  editPost(text, title, id) {
+    let array = this.state.postData;
+    console.log(array, " array");
+    for(let i = 0; i < array.length; i++) {
+      if (array[i].key === id) {
+        console.log(id, " id");
+        console.log(array[i].key, " i");
+        array[i].title = title;
+        array[i].text = text;
+      }
+    }
+    this.setState({postData: array});
   }
 
   render() {
     return (
         <div>
-          <button onClick={this.add.bind(null, 'Default text')} className="PostButton" id="add">Add</button>
-          { this.state.posts.map((text, i) => {
+          <button onClick={this.add.bind(null, 'Write your title here')} className="PostButton" id="add">Add</button>
+          {this.state.postData.map(item => {
             return (
-                <Post key={i} index={i} editPost={this.editPost} removePost={this.removePost.bind(this)}>
-                  {text}
-                </Post>
+              <Post
+                key={item.key}
+                index={item.key}
+                editPost={this.editPost}
+                removePost={this.removePost.bind(this)}
+                editing={item.editing}
+                title={item.title}
+                text={item.text}/>
             )
           })}
         </div>
